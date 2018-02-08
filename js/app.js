961594ef28d578bb38e18341ea1bca06b2cab5d3
LauraJH16 <laurajimenezh16@hotmail.com>
2018-02-08 06:02:12 -0500

    Añadiendo eslint y arreglando identacion

--- js/app.js
+++ js/app.js
@@ -1,32 +1,32 @@
 let begin = () => {
-    let inputOrigin = document.getElementById('origin');
-    let inputDestination = document.getElementById('destination');
-    let btnRout = document.getElementById('btn-trazar-ruta');
-    let getPosition = localizacion => {
-      let latitude = localizacion.coords.latitude;
-      console.log(latitude);
-      let longitude = localizacion.coords.longitude;
-      console.log(longitude);
-      const mapBox = document.getElementById('map');
-        let map = new google.maps.Map(mapBox,{
-            zoom: 15,
-            center: {
-                lat: latitude,
-                lng: longitude
-            }                      
-        }) 
-        let miUbicacion = new google.maps.Marker({
-			position: {lat:latitude, lng:longitude},
-			animation: google.maps.Animation.DROP,
-			map: map
-		});  
-    }
-    
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(getPosition);
-  
-      } else {
-          alert('La geolocalización no está disponible en el navegador')
-      }
+  let inputOrigin = document.getElementById('origin');
+  let inputDestination = document.getElementById('destination');
+  let btnRout = document.getElementById('btn-trazar-ruta');
+  let getPosition = localizacion => {
+    let latitude = localizacion.coords.latitude;
+    console.log(latitude);
+    let longitude = localizacion.coords.longitude;
+    console.log(longitude);
+    const mapBox = document.getElementById('map');
+    let map = new google.maps.Map(mapBox, {
+      zoom: 15,
+      center: {
+        lat: latitude,
+        lng: longitude
+      }                      
+    }); 
+    let miUbicacion = new google.maps.Marker({
+      position: {lat: latitude,
+        lng: longitude},
+      animation: google.maps.Animation.DROP,
+      map: map
+    });  
   };
-  window.addEventListener('load', begin)
+    
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(getPosition);
+  } else {
+    alert('La geolocalización no está disponible en el navegador');
+  }
+};
+window.addEventListener('load', begin);
